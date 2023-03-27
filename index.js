ab8dcd8441bb505923689c954c8000a4526ae174
Michael Johnson <Michaeljcantley@gmail.com>
2023-03-27 12:10:17 -0400

    Add color changing capability

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Create main container and add to the DOM
 
 const main = document.querySelector('.container');
+const colorContainer = document.querySelector('.color-container');
 
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
-    main.appendChild(row);
+    colorContainer.insertAdjacentElement('afterend', row);
 }
 
 const rows = document.querySelectorAll('.row');
@@ -19,6 +20,42 @@ Array.from(rows).forEach((row) => {
         row.appendChild(square);
     }
 })
+
+/* Set a group of colors on the top of the page that will
+   be able to be clicked, that changes the color of the 
+   trail. Pick 6 colors, and link them to helper classes
+   that have the same name as the colors. Also, don't allow
+   more than 1 color to be active at one time, to avoid conflicts. */
+
+   // Red, Blue, Orange, Brown, Black, Pink, Purple, Green
+
+   // Set a variable that captures the value of one of the color buttons
+
+   // Set an event listener that listens for the capture of one of the colors
+
+   const colors = document.querySelectorAll('button');
+   let myChoice = 'black';
+
+   Array.from(colors).forEach((color) => {
+        color.addEventListener('click', function() {
+            myChoice = this.id;
+        })
+   })
+
+/* Set an event listener on the squares that listens for a mouseover and possibly
+   a mouseout if I want the color to change back to a default. The mouseover will
+   change the class of the square that it hovers over. I want it to add the class
+   instead of toggling it as I don't want any errors to happen. */
+
+   const squares = document.querySelectorAll('.square');
+   Array.from(squares).forEach((square) => {
+    square.addEventListener('mouseover', function() {
+        square.classList.add(`${myChoice}`);
+    })
+   })
+   
+
+   
 
 
 
