57798829fe1dab019fbcbefb7188a83a09eb9372
Michael Johnson <Michaeljcantley@gmail.com>
2023-03-27 11:04:38 -0400

    Finish Trail page

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // Create main container and add to the DOM
 
+const main = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    main.appendChild(row);
+}
+
+const rows = document.querySelectorAll('.row');
+
+// Create 16 square cells for each row, resulting in 16x16 grid
+
+Array.from(rows).forEach((row) => {
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+})
+
 
 
 
