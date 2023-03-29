516142b0c548ef03f5758465abbcbe254a655918
Michael Johnson <Michaeljcantley@gmail.com>
2023-03-29 08:49:21 -0400

    Bundle Grid functionality

--- index.js
+++ index.js
@@ -2,24 +2,46 @@
 
 const main = document.querySelector('.container');
 const colorContainer = document.querySelector('.color-container');
+const retry = document.querySelector('.retry');
+const start = document.querySelector('.start');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    colorContainer.insertAdjacentElement('afterend', row);
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        colorContainer.insertAdjacentElement('afterend', row);
+    }
+    
+    const rows = document.querySelectorAll('.row');
+
+    Array.from(rows).forEach((row) => {
+        for (let i = 0; i < 16; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+    })
+    const colors = document.querySelectorAll('.color-container > button');
+    let myChoice;
+    
+    Array.from(colors).forEach((color) => {
+         color.addEventListener('click', function() {
+             myChoice = this.id;
+         })
+    })
+    const squares = document.querySelectorAll('.square');
+    const mySquares = Array.from(squares);
+    mySquares.forEach((square) => {
+     square.addEventListener('mouseover', function() {
+         square.classList.add(`${myChoice}`);
+     })
+    })
 }
 
-const rows = document.querySelectorAll('.row');
+start.addEventListener('click', createGrid);
 
 // Create 16 square cells for each row, resulting in 16x16 grid
 
-Array.from(rows).forEach((row) => {
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
-    }
-})
 
 /* Set a group of colors on the top of the page that will
    be able to be clicked, that changes the color of the 
@@ -33,26 +55,25 @@ Array.from(rows).forEach((row) => {
 
    // Set an event listener that listens for the capture of one of the colors
 
-   const colors = document.querySelectorAll('button');
-   let myChoice = 'black';
-
-   Array.from(colors).forEach((color) => {
-        color.addEventListener('click', function() {
-            myChoice = this.id;
-        })
-   })
 
 /* Set an event listener on the squares that listens for a mouseover and possibly
    a mouseout if I want the color to change back to a default. The mouseover will
    change the class of the square that it hovers over. I want it to add the class
    instead of toggling it as I don't want any errors to happen. */
 
-   const squares = document.querySelectorAll('.square');
-   Array.from(squares).forEach((square) => {
-    square.addEventListener('mouseover', function() {
-        square.classList.add(`${myChoice}`);
-    })
-   })
+
+   let amount = 0;
+
+   function retryGrid() {
+    amount = prompt('Enter an amount for the size of the grid');
+    rows.forEach((row) => row.remove());
+    squares.forEach((square) => square.remove());
+   }
+   
+   retry.addEventListener('click', retryGrid);
+
+   
+   
    
 
    
